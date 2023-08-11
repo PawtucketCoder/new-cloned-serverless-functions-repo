c8e755c1696c919aa8f336e6737bcd11e6e308f6
Michael <pawtucketcoder@gmail.com>
2023-08-10 23:16:36 -0400

    new logo

--- netlify/functions/verify-member.js
+++ netlify/functions/verify-member.js
@@ -21,58 +21,74 @@ function generateAccessToken(email, username) {
   return jwt.sign({ email, username }, process.env.TOKEN_SECRET, { expiresIn: '1h' }); // Set an appropriate expiration time
 }
 
+// ... (other imports and code)
+
 export const handler = async (event) => {
-  try {
-    const body = JSON.parse(event.body);
-    const { email, verificationCode } = body;
-
-    // Check if the member already exists in the database
-    const existingMember = await new Promise((resolve, reject) => {
-      db.query('SELECT * FROM members WHERE email=?', [email],
-        function (err, results, fields) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(results);
+    try {
+      const body = JSON.parse(event.body);
+      const { email, verificationCode } = body;
+  
+      // Check if the member already exists in the database
+      const existingMember = await new Promise((resolve, reject) => {
+        db.query('SELECT * FROM members WHERE email=?', [email],
+          function (err, results, fields) {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(results);
+            }
           }
+        );
+      });
+  
+      if (existingMember.length > 0) {
+        // Member exists, compare the hashed password
+        const hashedVerificationCode = existingMember[0].verification_code;
+        const username = existingMember[0].username;
+        const isVerificationCodeCorrect = await bcrypt.compare(verificationCode, hashedVerificationCode);
+        const tokenValue = generateAccessToken(email, username);
+  
+        if (isVerificationCodeCorrect) {
+          // Update is_verified field in the database
+          await new Promise((resolve, reject) => {
+            db.query('UPDATE members SET is_verified = 1 WHERE email = ?', [email],
+              function (err, results, fields) {
+                if (err) {
+                  reject(err);
+                } else {
+                  resolve(results);
+                }
+              }
+            );
+          });
+  
+          // Password is correct, return a JWT access token
+          return {
+            statusCode: 200,
+            headers: {
+              'Set-Cookie': `token=${tokenValue}; Path=/`
+            },
+            body: JSON.stringify({ message: 'Verification Code Accepted' })
+          };
+        } else {
+          // Password is incorrect, return an error
+          return {
+            statusCode: 401,
+            body: JSON.stringify({ message: 'Incorrect Verification Code' })
+          };
         }
-      );
-    });
-
-    if (existingMember.length > 0) {
-      // Member exists, compare the hashed password
-      const hashedVerificationCode = existingMember[0].verification_code;
-      const username = existingMember[0].username;
-      const isVerificationCodeCorrect = await bcrypt.compare(verificationCode, hashedVerificationCode);
-      const tokenValue = generateAccessToken(email, username);
-
-      if (isVerificationCodeCorrect) {
-        // Password is correct, return a JWT access token
-        return {
-          statusCode: 200,
-          headers: {
-            'Set-Cookie': `token=${tokenValue}; Path=/`
-          },
-          body: JSON.stringify({ message: 'Verification Code Accepted' })
-        };
       } else {
-        // Password is incorrect, return an error
+        // Member does not exist, return an error
         return {
           statusCode: 401,
-          body: JSON.stringify({ message: 'Incorrect Verification Code' })
+          body: JSON.stringify({ message: 'Member Verification Denied' })
         };
       }
-    } else {
-      // Member does not exist, return an error
+    } catch (error) {
       return {
-        statusCode: 401,
-        body: JSON.stringify({ message: 'Member Verification Denied' })
+        statusCode: 500,
+        body: JSON.stringify(error)
       };
     }
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify(error)
-    };
-  }
-};
+  };
+  
